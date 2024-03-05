a73c72258b6d41e49e6f48ed3f6a4135a79d52dc
Felipe Vasconcelos <felipevasconceloslpz@gmail.com>
2024-03-05 10:48:42 -0300

    first commit

---
+++ upload.py
@@ -0,0 +1,22 @@
+import os
+import boto3
+SPACES_KEY = "DO00LNFAEQRLTA82CJCM"
+SPACES_SECRET = "cOWHXzPEeesRVZSJDp/maav9CcnqYyyaOFh2HNWHcJ0"
+
+session = boto3.session.Session()
+client = session.client('s3',
+                        region_name='nyc3',
+                        endpoint_url='https://nyc3.digitaloceanspaces.com',
+                        aws_access_key_id=SPACES_KEY,
+                        aws_secret_access_key=SPACES_SECRET)
+
+
+# files_list = []
+for city in ["rj","sp","df","cwb"]:
+    path = f"/home/felipe/dados_vm/{city}"
+    for number,filename in enumerate(os.listdir(path)):
+        # files_list.append(filename)
+        client.upload_file(f"/home/felipe/dados_vm/{city}/{filename}",'brbus-dataset',f'bronze/{city}/{filename}')
+        print(number)
+
+    
